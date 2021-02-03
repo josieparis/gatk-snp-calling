1189bedf2f226abd4c3337e6c078eccde86ab35e
Josie Paris <38511308+josieparis@users.noreply.github.com>
2021-02-03 13:56:59 +0000

    Update 5_merge_sample_bams.sh

--- SNP_calling/scripts/5_merge_sample_bams.sh
+++ SNP_calling/scripts/5_merge_sample_bams.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=02:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=2
-#SBATCH -A Research_Project-T110748
+#SBATCH -A <research_project>
 #SBATCH --job-name=master_merge_bams 
 #SBATCH --error=master_merge_bams_C.err.txt 
 #SBATCH --output=master_merge_bams_C.out.txt 
@@ -16,16 +16,20 @@
 
 ## Load your system modules
 # Required modules are: picard tools
+module purge
 module load picard/2.6.0-Java-1.8.0_131
 
 ## Set your master path
-MASTER=/gpfs/ts0/home/jrp228/NERC/people/josie/github_test/gatk-snp-calling
+MASTER=<path>
 
 ## Fill in directories if different from the workspace setup
 bam_path=$MASTER/SNP_calling/bams/interim_bams
 
 ## Fill in path for population specific metadata
 metadata=$MASTER/SNP_calling/metadata.tsv
+
+## Path to picard:
+EBROOTPICARD=<path_to_picard>
 
 ### The sample IDs are extracted from the metadata
 i=$(cut -f 1 $matedata | tail -n+2 | uniq | sed -n "${SLURM_ARRAY_TASK_ID}p")
