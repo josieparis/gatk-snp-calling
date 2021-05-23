064174a5eea5cf3e6961363cd97ec1c8f42e078e
Josie Paris <38511308+josieparis@users.noreply.github.com>
2021-05-23 13:54:38 +0200

    Update 5_merge_sample_bams.sh

--- SNP_calling/scripts/5_merge_sample_bams.sh
+++ SNP_calling/scripts/5_merge_sample_bams.sh
@@ -6,8 +6,8 @@
 #SBATCH --ntasks-per-node=2
 #SBATCH -A <research_project>
 #SBATCH --job-name=master_merge_bams 
-#SBATCH --error=master_merge_bams_C.err.txt 
-#SBATCH --output=master_merge_bams_C.out.txt 
+#SBATCH --error=master_merge_bams.err.txt 
+#SBATCH --output=master_merge_bams.out.txt 
 #SBATCH --export=All
 #SBATCH --array=1-40%10
 
@@ -32,7 +32,7 @@ metadata=$MASTER/SNP_calling/metadata.tsv
 EBROOTPICARD=<path_to_picard>
 
 ### The sample IDs are extracted from the metadata
-i=$(cut -f 1 $matedata | tail -n+2 | uniq | sed -n "${SLURM_ARRAY_TASK_ID}p")
+i=$(cut -f 1 $metadata | tail -n+2 | uniq | sed -n "${SLURM_ARRAY_TASK_ID}p")
 
 # Find all bams for this individual and merge
     mfiles=`ls -1 ${bam_path}/${i}_*sorted.dups.bam`
